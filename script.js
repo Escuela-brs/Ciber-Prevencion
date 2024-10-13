66e3adfe7e86870b167d1f35b4580b24e03bd3bd
Escuela-brs <ya.no.se.que.666@gmail.com>
2024-10-13 16:46:29 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,27 +1,3 @@
-/**/
-
-document.getElementById('scrollToTop').addEventListener('click', function() {
-    window.scrollTo({
-        top: 0,
-        behavior: 'smooth'
-    });
-});
-
-function smoothScroll(event, targetId) {
-      event.preventDefault();
-      const targetElement = document.getElementById(targetId);
-      if (targetElement) {
-        window.scrollTo({
-        top: targetElement.offsetTop,
-        behavior: 'smooth'
-      });
-    } else {
-        console.warn('Elemento con ID "' + targetId + '" no encontrado.');
-      }
-}
-
-/*  */
-
 const nav = document.querySelector('#nav_uno');
 const abrir = document.querySelector('#abrir');
 const cerrar = document.querySelector('#cerrar');
@@ -48,3 +24,58 @@ document.addEventListener('click', (event) => {
         cerrarMenu();
     }
 });
+
+        
+document.addEventListener('DOMContentLoaded', () => {
+  document.querySelectorAll('.preg_fre').forEach(button => {
+    button.addEventListener('click', () => {
+      const content = button.nextElementSibling;
+      content.classList.toggle('hidden_preg');
+      const icon = button.querySelector('span:last-child');
+      icon.textContent = icon.textContent === '+' ? '−' : '+';
+    });
+  });
+});
+
+
+const btn = document.getElementById("cont_btn")
+
+document.getElementById("contactForm").addEventListener("submit", function (event) {
+  event.preventDefault()
+
+  btn.value = "Enviando..."
+
+  const serviceID = "default_service"
+  const templateID = "template_cdc0ooo"
+
+  emailjs.sendForm(serviceID, templateID, this).then(
+    () => {
+      btn.value = "Enviar"
+    },
+    (err) => {
+      btn.value = "Enviar"
+      alert(JSON.stringify(err))
+    },
+  )
+})
+
+
+document.getElementById('scrollToTop').addEventListener('click', function() {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
+});
+
+function smoothScroll(event, targetId) {
+      event.preventDefault();
+      const targetElement = document.getElementById(targetId);
+      if (targetElement) {
+        window.scrollTo({
+        top: targetElement.offsetTop,
+        behavior: 'smooth'
+      });
+    } else {
+        console.warn('Elemento con ID "' + targetId + '" no encontrado.');
+      }
+}
